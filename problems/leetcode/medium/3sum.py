5ce152690d24a2e78de96cb9c48305a6bfb32a0f
Farhan Ali <farhanalicar@gmail.com>
2025-01-21 18:52:15 -0800

    cleaning working branch

---
+++ problems/leetcode/medium/3sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pass 
+        def combinations(arr):
+            
+        # get all set of all combinations len(nums) choose 3
+        # double iteration find which ones add up to each other
+
+
+
+test = Solution()
+
+assert test.threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
